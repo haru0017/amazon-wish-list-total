b517a3002ac74222980e942d6fb2e303964b256b
Tanaka Haruto <hammerhead0017@gmail.com>
2021-01-08 22:37:37 +0900

    bugの修正

--- js/list-total.js
+++ js/list-total.js
@@ -1,7 +1,8 @@
-let total = 0
-let el = document.getElementsByClassName('a-price-whole')
+let total = 0;
+let el = document.getElementsByClassName("a-price-whole");
 for (let i = 0; i < el.length; i++) {
-    total += parseInt(el[i].innerHTML.replace(",", ""))
+  total += parseInt(el[i].innerHTML.replace(",", ""));
 }
-let list_name = document.getElementById('profile-list-name')
-list_name.innerHTML = list_name.innerHTML + ' ( ' + total.toLocaleString() + '円 ) '
+let list_name = document.getElementById("profile-list-name");
+list_name.innerHTML =
+  list_name.innerHTML + " ( " + total.toLocaleString() + "円 ) ";
